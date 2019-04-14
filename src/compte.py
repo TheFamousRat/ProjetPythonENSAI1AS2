c9f8c7a1f7e92edb75e291fb4924f8bd742c3b5d
Guillaume Roy <gpotato.gr@gmail.com>
2019-04-14 17:13:23 +0200

    Ajout de fonction input
    Donne les options en fonction de la (non)connection et des droits du l'utilisateur

--- src/compte.py
+++ src/compte.py
@@ -8,11 +8,11 @@ Created on Tue Apr  2 11:18:32 2019
 class Compte:
     motDePasse = ""
     pseudo = ""
-    administrateur = False #Administrateur ou collaborateur
+    admin = False #Administrateur ou collaborateur
     
     def __init__(self, pseudo_ : str, motDePasse_ : str, administrateur_ : bool = False):
         self.pseudo = pseudo_
         self.motDePasse = motDePasse_
-        self.administrateur = administrateur_
+        self.admin = administrateur_
         
     
--- src/main.py
+++ src/main.py
@@ -9,14 +9,60 @@ Fichier principal du programme
 
 from compte import Compte
 from baseUtilisateur import BaseUtilisateur
+from catalogue import Catalogue
 
-pseudoUtilisateur = None #Pseudo de l'utilisateur actuel
+compteUtilisateur = Compte("Boris","1234", False) #Pseudo de l'utilisateur actuel
+catalogueOFF = Catalogue() #OFF = Open Food Facts
+baseUsers = BaseUtilisateur()
 
-testBase = BaseUtilisateur()
-testBase.ajouterCompte(Compte("gérardLeFat","1234"))
-print(testBase.identifiantsCorrects("gérardeFat","1234"))
+NOMBRE_DE_CHOIX = 0
 
-def input():
-    continuer = True
-    while (continuer):
-        #....
+def main():
+	init() # On initialise le code
+	
+	continuer = True
+	while (continuer):
+		print("Bienvenue dans la base OFF.")
+		if compteUtilisateur:
+			print("Vous êtes connecté sous le pseudo " + str(compteUtilisateur.pseudo) + (" (admin)." if compteUtilisateur.admin else "."))
+		else:
+			print("Vous n'êtes pas connecté")
+		print("\nTâches disponibles :")
+		#On va maintenant afficher les tâches en fonction de la connexion ou non de l'utilisateur (et de ses droits)
+		print(" 1/Afficher un produit")
+		print(" 2/Afficher les ingrédients d'un produit")
+		if compteUtilisateur == None:#L'utilisateur n'est pas connecté
+			print(" 3/Création de compte")
+			print(" 4/Connection")
+			NOMBRE_DE_CHOIX = 4
+		else:
+			print(" 3/Afficher n premiers produits ajoutés")
+			print(" 4/Créer ou modifier un produit")
+			print(" 5/Déconnexion")
+			if compteUtilisateur.admin:
+				print(" 6/Supprimer un produit")
+				print(" 7/Valider ou supprimer un compte contributeur")
+				print(" 8/Deux statistiques au choix parmi trois")
+				print(" 9/Un graphique au choix parmi deux")
+				NOMBRE_DE_CHOIX = 9
+			else:
+				NOMBRE_DE_CHOIX = 5
+		
+		nombreRecu = ""
+		while nombreRecu == "":
+			nombreRecu = input("Entrez votre choix : ")
+			try:
+				nombreRecu = int(nombreRecu)
+				if nombreRecu < 0 or nombreRecu > NOMBRE_DE_CHOIX:
+					print("Erreur : choix inconnu")
+					nombreRecu = ""
+			except ValueError:
+				print("Erreur : type de variable incompatible (entrez un entier)")
+				nombreRecu = ""
+			
+	
+def init():
+	catalogueOFF.chargerBase("../data/open_food_facts.csv")
+	baseUsers.ajouterCompte(Compte("FredPoul","jesuisvieux",False))
+	
+main()
