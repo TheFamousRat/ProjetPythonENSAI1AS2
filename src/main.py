5532aea0c32b6198a1a7d9b5d3e7c9c3f887c24f
Guillaume Roy <gpotato.gr@gmail.com>
2019-04-14 17:35:57 +0200

    Modification obtention nombre choisi

--- src/main.py
+++ src/main.py
@@ -15,8 +15,6 @@ compteUtilisateur = Compte("Boris","1234", False) #Pseudo de l'utilisateur actue
 catalogueOFF = Catalogue() #OFF = Open Food Facts
 baseUsers = BaseUtilisateur()
 
-NOMBRE_DE_CHOIX = 0
-
 def main():
 	init() # On initialise le code
 	
@@ -29,12 +27,14 @@ def main():
 			print("Vous n'êtes pas connecté")
 		print("\nTâches disponibles :")
 		#On va maintenant afficher les tâches en fonction de la connexion ou non de l'utilisateur (et de ses droits)
+		nombreChoisi = 0
+		
 		print(" 1/Afficher un produit")
 		print(" 2/Afficher les ingrédients d'un produit")
 		if compteUtilisateur == None:#L'utilisateur n'est pas connecté
 			print(" 3/Création de compte")
 			print(" 4/Connection")
-			NOMBRE_DE_CHOIX = 4
+			nombreChoisi = obtenirChoix(4)
 		else:
 			print(" 3/Afficher n premiers produits ajoutés")
 			print(" 4/Créer ou modifier un produit")
@@ -44,22 +44,24 @@ def main():
 				print(" 7/Valider ou supprimer un compte contributeur")
 				print(" 8/Deux statistiques au choix parmi trois")
 				print(" 9/Un graphique au choix parmi deux")
-				NOMBRE_DE_CHOIX = 9
+				nombreChoisi = obtenirChoix(9)
 			else:
-				NOMBRE_DE_CHOIX = 5
-		
-		nombreRecu = ""
-		while nombreRecu == "":
-			nombreRecu = input("Entrez votre choix : ")
-			try:
-				nombreRecu = int(nombreRecu)
-				if nombreRecu < 0 or nombreRecu > NOMBRE_DE_CHOIX:
-					print("Erreur : choix inconnu")
-					nombreRecu = ""
-			except ValueError:
-				print("Erreur : type de variable incompatible (entrez un entier)")
+				nombreChoisi = obtenirChoix(5)
+
+				
+def obtenirChoix(nombreMaxChoix : int) -> int:
+	nombreRecu = ""
+	while nombreRecu == "":
+		nombreRecu = input("Entrez votre choix : ")
+		try:
+			nombreRecu = int(nombreRecu)
+			if nombreRecu < 0 or nombreRecu > nombreMaxChoix:
+				print("Erreur : choix inconnu")
 				nombreRecu = ""
-			
+		except ValueError:
+			print("Erreur : type de variable incompatible (entrez un entier)")
+			nombreRecu = ""
+	return nombreRecu
 	
 def init():
 	catalogueOFF.chargerBase("../data/open_food_facts.csv")
