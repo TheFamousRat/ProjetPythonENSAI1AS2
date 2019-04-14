20f414d276f54b42bd35cd99ff85925e00c9cb92
ROY Guillaume <id1262@DOMENSAI.ECOLE>
2019-04-14 19:33:32 +0200

    Refonte de l'organisation des choix, ajout de fonctions de déconnexion/quitter le programme

--- src/main.py
+++ src/main.py
@@ -10,61 +10,111 @@ Fichier principal du programme
 from compte import Compte
 from baseUtilisateur import BaseUtilisateur
 from catalogue import Catalogue
+from enum import Enum
 
-compteUtilisateur = Compte("Boris","1234", False) #Pseudo de l'utilisateur actuel
-catalogueOFF = Catalogue() #OFF = Open Food Facts
-baseUsers = BaseUtilisateur()
+class Choix:#Une simple classe qui contient la description associée à un choix, ainsi que la fonction appellée
+    def __init__(self, desc, fonc):
+        self.description = desc
+        self.fonction = fonc
 
-def main():
-	init() # On initialise le code
-	
-	continuer = True
-	while (continuer):
-		print("Bienvenue dans la base OFF.")
-		if compteUtilisateur:
-			print("Vous êtes connecté sous le pseudo " + str(compteUtilisateur.pseudo) + (" (admin)." if compteUtilisateur.admin else "."))
-		else:
-			print("Vous n'êtes pas connecté")
-		print("\nTâches disponibles :")
-		#On va maintenant afficher les tâches en fonction de la connexion ou non de l'utilisateur (et de ses droits)
-		nombreChoisi = 0
-		
-		print(" 1/Afficher un produit")
-		print(" 2/Afficher les ingrédients d'un produit")
-		if compteUtilisateur == None:#L'utilisateur n'est pas connecté
-			print(" 3/Création de compte")
-			print(" 4/Connection")
-			nombreChoisi = obtenirChoix(4)
-		else:
-			print(" 3/Afficher n premiers produits ajoutés")
-			print(" 4/Créer ou modifier un produit")
-			print(" 5/Déconnexion")
-			if compteUtilisateur.admin:
-				print(" 6/Supprimer un produit")
-				print(" 7/Valider ou supprimer un compte contributeur")
-				print(" 8/Deux statistiques au choix parmi trois")
-				print(" 9/Un graphique au choix parmi deux")
-				nombreChoisi = obtenirChoix(9)
-			else:
-				nombreChoisi = obtenirChoix(5)
-
-				
-def obtenirChoix(nombreMaxChoix : int) -> int:
-	nombreRecu = ""
-	while nombreRecu == "":
-		nombreRecu = input("Entrez votre choix : ")
-		try:
-			nombreRecu = int(nombreRecu)
-			if nombreRecu < 0 or nombreRecu > nombreMaxChoix:
-				print("Erreur : choix inconnu")
-				nombreRecu = ""
-		except ValueError:
-			print("Erreur : type de variable incompatible (entrez un entier)")
-			nombreRecu = ""
-	return nombreRecu
-	
-def init():
-	catalogueOFF.chargerBase("../data/open_food_facts.csv")
-	baseUsers.ajouterCompte(Compte("FredPoul","jesuisvieux",False))
-	
-main()
+class Main:
+    def main(self):
+        
+        print("Bienvenue dans la base OFF.")
+        while (self.continuer):
+            
+            if self.compteUtilisateur:
+                print("Vous êtes connecté sous le pseudo " + str(self.compteUtilisateur.pseudo) + (" (admin)." if self.compteUtilisateur.admin else "."))
+            else:
+                print("Vous n'êtes pas connecté")
+            
+            #On actualise le type de l'utilisateur
+            if self.compteUtilisateur == None:
+                typeUtilisateur = "utilisateur"
+            elif self.compteUtilisateur.admin:
+                typeUtilisateur = "admin"
+            else:
+                typeUtilisateur = "contributeur"
+    
+            print("\nTâches disponibles :")
+            self.menuChoix(typeUtilisateur)
+            nombreRecu = self.obtenirChoix(typeUtilisateur)
+            self.appliquerChoix(typeUtilisateur, nombreRecu)
+            print(" ")
+           
+    def menuChoix(self, indexDicChoix : str):#Fonction affichant les choix disponibles à l'utilisateur
+        for i in range(len(self.choixDispo[indexDicChoix])):
+            print(" " + str(i) + "/" + self.choixDispo[indexDicChoix][i].description)
+    
+    def appliquerChoix(self, typeUtilisateur : str, choix : int):
+        self.choixDispo[typeUtilisateur][choix].fonction()#On appelle la fonction liée au choix
+            
+    def obtenirChoix(self, typeUtilisateur : str) -> int:
+        nombreChoix = len(self.choixDispo[typeUtilisateur]) - 1
+        nombreRecu = ""
+        while nombreRecu == "":
+            nombreRecu = input("Entrez votre choix : ")
+            try:
+                nombreRecu = int(nombreRecu)
+                if nombreRecu < 0 or nombreRecu > nombreChoix:
+                    print("Erreur : choix inconnu")
+                    nombreRecu = ""
+            except ValueError:
+                print("Erreur : type de variable incompatible (entrez un entier)")
+                nombreRecu = ""
+        return nombreRecu
+    	
+    def __init__(self):
+        
+        self.continuer = True
+        self.choixDispo = {"utilisateur" : [], "contributeur" : [], "admin" : []} #Dictionnaires contenant les choix associés à chaque type d'utilisateur
+        self.compteUtilisateur = Compte("Boris","1234", False) #Pseudo de l'utilisateur actuel
+        self.typeUtilisateur = "" #Un des 3 mentionnés au-dessus, utilisateur, contributeur ou admin
+        self.catalogueOFF = Catalogue() #OFF = Open Food Facts
+        self.baseUsers = BaseUtilisateur() #La base utilisateur
+        
+        #On initialise d'abord la base de données OFF
+        self.catalogueOFF.chargerBase("../data/open_food_facts.csv")
+        #Puis la base utilisateur
+        self.baseUsers.ajouterCompte(Compte("FredPoul","jesuisvieux",False))
+        #Puis enfin les choix disponibles, qu'on lie à leurs fonctions respectives
+        self.choixDispo["utilisateur"] = []
+        self.choixDispo["contributeur"] = []
+        self.choixDispo["admin"] = []
+        #Les choix si l'utilisateur est non connecté
+        self.choixDispo["utilisateur"].append(Choix("Afficher un produit", self.dummy))
+        self.choixDispo["utilisateur"].append(Choix("Afficher les ingrédients d'un produit", self.dummy))
+        self.choixDispo["utilisateur"].append(Choix("Création de compte", self.dummy))
+        self.choixDispo["utilisateur"].append(Choix("Connexion", self.dummy))
+        self.choixDispo["utilisateur"].append(Choix("Quitter", self.quitter))
+        #Si il est contributeur
+        self.choixDispo["contributeur"].append(Choix("Afficher un produit", self.dummy))
+        self.choixDispo["contributeur"].append(Choix("Afficher les ingrédients d'un produit", self.dummy))
+        self.choixDispo["contributeur"].append(Choix("Afficher n premiers produits ajoutés", self.dummy))
+        self.choixDispo["contributeur"].append(Choix("Créer ou modifier un produit", self.dummy))
+        self.choixDispo["contributeur"].append(Choix("Déconnexion", self.deconnecter))
+        self.choixDispo["contributeur"].append(Choix("Quitter", self.quitter))
+        #Si il est admin
+        self.choixDispo["admin"].append(Choix("Afficher un produit", self.dummy))
+        self.choixDispo["admin"].append(Choix("Afficher les ingrédients d'un produit", self.dummy))
+        self.choixDispo["admin"].append(Choix("Afficher n premiers produits ajoutés", self.dummy))
+        self.choixDispo["admin"].append(Choix("Créer ou modifier un produit", self.dummy))
+        self.choixDispo["admin"].append(Choix("Supprimer un produit", self.dummy))
+        self.choixDispo["admin"].append(Choix("Valider ou supprimer un compte contributeur", self.dummy))
+        self.choixDispo["admin"].append(Choix("Deux statistiques au choix parmi trois", self.dummy))
+        self.choixDispo["admin"].append(Choix("Un graphique au choix parmi deux", self.dummy))
+        self.choixDispo["admin"].append(Choix("Déconnexion", self.deconnecter))
+        self.choixDispo["admin"].append(Choix("Quitter", self.quitter))
+    	
+    def dummy(self):
+        print("FONCTION NON-TERMINEE")
+        
+    def deconnecter(self):
+        print("Déconnexion")
+        self.compteUtilisateur = None
+        
+    def quitter(self):
+        self.continuer = False
+    
+main = Main()
+main.main()
